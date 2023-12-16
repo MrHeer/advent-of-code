d23e416b43d101083b74f37271e224b330a9d6a7
He Linming <hlm52pk@163.com>
2023-12-16 19:45:19 +0800

    feat: solved day 11

---
+++ src/bin/11.rs
@@ -0,0 +1,139 @@
+advent_of_code::solution!(11);
+
+#[derive(PartialEq)]
+enum Space {
+    Empty,
+    Galaxy,
+}
+
+use std::collections::HashSet;
+
+use Space::*;
+
+impl Space {
+    fn new(ch: &char) -> Option<Self> {
+        match ch {
+            '.' => Some(Empty),
+            '#' => Some(Galaxy),
+            _ => None,
+        }
+    }
+}
+
+struct Position {
+    row: u64,
+    col: u64,
+}
+
+struct Image {
+    galaxies: Vec<Position>,
+    galaxy_rows: HashSet<u64>,
+    galaxy_cols: HashSet<u64>,
+}
+
+impl Image {
+    fn new(image: &str) -> Self {
+        let mut galaxies = vec![];
+        let (mut galaxy_rows, mut galaxy_cols) = (HashSet::new(), HashSet::new());
+        let (mut row, mut col) = (0, 0);
+
+        image.lines().for_each(|line| {
+            row += 1;
+            col = 0;
+            line.chars().for_each(|ch| {
+                col += 1;
+                let space = Space::new(&ch).unwrap();
+                if space == Galaxy {
+                    galaxies.push(Position { row, col });
+                    galaxy_rows.insert(row);
+                    galaxy_cols.insert(col);
+                }
+            });
+        });
+
+        Self {
+            galaxies,
+            galaxy_rows,
+            galaxy_cols,
+        }
+    }
+
+    fn get_shortest_path_length(
+        &self,
+        galaxy: &Position,
+        other_galaxy: &Position,
+        expansion: u64,
+    ) -> u64 {
+        let Position {
+            row: galaxy_row,
+            col: galaxy_col,
+        } = *galaxy;
+        let Position {
+            row: other_galaxy_row,
+            col: other_galaxy_col,
+        } = *other_galaxy;
+
+        let mut steps = 0;
+
+        let get_steps = |a: u64, b: u64, should_expand: &dyn Fn(&u64) -> bool| {
+            let min = Ord::min(a, b);
+            let max = Ord::max(a, b);
+            (min + 1..=max)
+                .map(|row| if should_expand(&row) { 1 } else { expansion })
+                .sum::<u64>()
+        };
+
+        steps += get_steps(galaxy_row, other_galaxy_row, &|row| {
+            self.galaxy_rows.contains(row)
+        });
+
+        steps += get_steps(galaxy_col, other_galaxy_col, &|col| {
+            self.galaxy_cols.contains(col)
+        });
+
+        steps
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let image = Image::new(input);
+    let galaxies = &image.galaxies;
+    let galaxies_len = galaxies.len();
+    let mut sum = 0;
+    for i in 0..galaxies_len - 1 {
+        for j in i + 1..galaxies_len {
+            sum += image.get_shortest_path_length(&galaxies[i], &galaxies[j], 2)
+        }
+    }
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let image = Image::new(input);
+    let galaxies = &image.galaxies;
+    let galaxies_len = galaxies.len();
+    let mut sum = 0;
+    for i in 0..galaxies_len - 1 {
+        for j in i + 1..galaxies_len {
+            sum += image.get_shortest_path_length(&galaxies[i], &galaxies[j], 1000000)
+        }
+    }
+    Some(sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(374));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(82000210));
+    }
+}
